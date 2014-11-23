5c37b46045bdb148ccb27d4e6bcb0a40b9f71a34
Angythub <reyno.willy@gmail.com>
2014-11-23 23:54:46 +0100

    Add IntegerValidatorTest

--- tests/IntegerValidatorTest.php
+++ tests/IntegerValidatorTest.php
@@ -17,11 +17,46 @@ class IntegerValidatorTest extends \PHPUnit_Framework_TestCase
 
     public function testIntegerValidatorHigher()
     {
-        $higher = mt_rand(50, 100);
+        $highest = mt_rand(50, 100);
         $lowest = mt_rand(0, 49);
-        $higher = IntegerValidator::higher($higher,$lowest);
+        $higher = IntegerValidator::higher($highest,$lowest);
 
         $this->assertEquals($higher, true);
+    }
+
+    public function testIntegerValidatorLower()
+    {
+        $lowest = mt_rand(0, 49);
+        $highest = mt_rand(50, 100);
+        $lower = IntegerValidator::lower($lowest,$highest);
+
+        $this->assertEquals($lower, true);
+    }
+
+    public function testIntegerValidatorBetween()
+    {
+        $integer = mt_rand(33, 66);
+        $min = mt_rand(0, 32);
+        $max = mt_rand(67, 100);
+        $between = IntegerValidator::between($integer, $min, $max);
+
+        $this->assertEquals($between, true);
+    }
+
+    public function testIntegerValidatorNegative()
+    {
+        $integer = mt_rand(-100, -1);
+        $negative = IntegerValidator::negative($integer, true);
+
+        $this->assertEquals($negative, true);
+    }
+
+    public function testIntegerValidatorPositive()
+    {
+        $integer = mt_rand(1, 100);
+        $positive = IntegerValidator::negative($integer, true);
+
+        $this->assertEquals($positive, true);
     }
 
 } 
