cc32625790682c2bd3157c260885b9eba550d183
Angythub <reyno.willy@gmail.com>
2014-11-23 23:57:09 +0100

    Edit IntegerValidatorTest

--- tests/IntegerValidatorTest.php
+++ tests/IntegerValidatorTest.php
@@ -10,27 +10,27 @@ class IntegerValidatorTest extends \PHPUnit_Framework_TestCase
     public function testIntegerValidatorEqual()
     {
         $number = mt_rand(0, 10);
-        $equal = IntegerValidator::equal($number, $number);
+        $result = IntegerValidator::equal($number, $number);
 
-        $this->assertEquals($equal, true);
+        $this->assertEquals($result, true);
     }
 
     public function testIntegerValidatorHigher()
     {
         $highest = mt_rand(50, 100);
         $lowest = mt_rand(0, 49);
-        $higher = IntegerValidator::higher($highest,$lowest);
+        $result = IntegerValidator::higher($highest,$lowest);
 
-        $this->assertEquals($higher, true);
+        $this->assertEquals($result, true);
     }
 
     public function testIntegerValidatorLower()
     {
         $lowest = mt_rand(0, 49);
         $highest = mt_rand(50, 100);
-        $lower = IntegerValidator::lower($lowest,$highest);
+        $result = IntegerValidator::lower($lowest,$highest);
 
-        $this->assertEquals($lower, true);
+        $this->assertEquals($result, true);
     }
 
     public function testIntegerValidatorBetween()
@@ -38,25 +38,25 @@ class IntegerValidatorTest extends \PHPUnit_Framework_TestCase
         $integer = mt_rand(33, 66);
         $min = mt_rand(0, 32);
         $max = mt_rand(67, 100);
-        $between = IntegerValidator::between($integer, $min, $max);
+        $result = IntegerValidator::between($integer, $min, $max);
 
-        $this->assertEquals($between, true);
+        $this->assertEquals($result, true);
     }
 
     public function testIntegerValidatorNegative()
     {
         $integer = mt_rand(-100, -1);
-        $negative = IntegerValidator::negative($integer, true);
+        $result = IntegerValidator::negative($integer, true);
 
-        $this->assertEquals($negative, true);
+        $this->assertEquals($result, true);
     }
 
     public function testIntegerValidatorPositive()
     {
         $integer = mt_rand(1, 100);
-        $positive = IntegerValidator::positive($integer, true);
+        $result = IntegerValidator::positive($integer, true);
 
-        $this->assertEquals($positive, true);
+        $this->assertEquals($result, true);
     }
 
 } 
